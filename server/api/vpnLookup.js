1376d0545a60d7857fd3f70860e462ae49da7da2
Sudo-Self <sudo-self@proton.me>
2024-11-07 00:11:25 -0700

    Update vpnLookup.js

--- server/api/vpnLookup.js
+++ server/api/vpnLookup.js
@@ -1,16 +1,14 @@
-// server/api/vpnLookup.js
-import { defineEventHandler, getQuery, sendError } from 'h3';
+import { defineEventHandler, readBody, sendError } from 'h3';
 
 export default defineEventHandler(async (event) => {
-  const { ip_address } = getQuery(event);
+  const { ip } = await readBody(event);
 
-  if (!ip_address) {
+  if (!ip) {
     return sendError(event, createError({ statusCode: 400, statusMessage: 'IP address is required' }));
   }
 
   const apiKey = process.env.VPN_LOOKUP_API_KEY;
-
-  const url = `https://api.vpnlookup.com/v1/ip/${ip_address}?apiKey=${apiKey}`;
+  const url = `https://api.vpnlookup.com/v1/${ip}?apiKey=${apiKey}`;
 
   try {
     const response = await $fetch(url);
@@ -19,3 +17,4 @@ export default defineEventHandler(async (event) => {
     return sendError(event, createError({ statusCode: error.response?.status || 500, statusMessage: error.message }));
   }
 });
+
